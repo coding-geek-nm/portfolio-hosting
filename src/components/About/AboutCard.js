acdcac3bd7468f5aa5e7561affd342d13b9c9fec
coding-geek-nm <nidhi.ci22@bmsce.ac.in>
2025-07-23 15:19:58 +0530

    Initial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">NIDHI B M </span>
+            from <span className="purple"> Karnataka, India.</span>
+            <br />
+            I am currently a final year Undergrad,
+            <br />
+            from BMS College of Engineering,Banglore.
+            
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Cooking
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Listening to songs
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Tiny Things Can Make a Big Impact!"{" "}
+          </p>
+          <footer className="blockquote-footer">Nidhi</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,103 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+// import leaf from "../../Assets/Projects/leaf.png";
+// import emotion from "../../Assets/Projects/emotion.png";
+// import editor from "../../Assets/Projects/codeEditor.png";
+// import chatify from "../../Assets/Projects/chatify.png";
+// import suicide from "../../Assets/Projects/suicide.png";
+// import bitsOfCode from "../../Assets/Projects/blog.png";
+import icb from "../../Assets/icbwebsite.jpg";
+import iot from "../../Assets/iot.jpg"
+import chatroom from "../../Assets/chatroom.png"
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={icb}
+              isBlog={false}
+              title="ICB Website"
+              description="•	Developed a secure ICB website featuring login/logout functionality with restricted access for ICB department students and robust security measures to maintain data integrity."
+              ghLink="https://github.com/coding-geek-nm/department-website-frontend"
+              // demoLink="https://chatify-49.web.app/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={iot}
+              isBlog={false}
+              title="Automatic Railwaygate Handler"
+              description="•	Developed an Automatic Railway Gate Handler using an ESP32 Wi-Fi module, ultrasonic sensor, and servo motor for automated gate control based on distance measurements.
+•	Created a Mobile App in Java to interface with the ESP32 via Wi-Fi, allowing remote gate operation and real-time status updates.•	Implemented Sensor and Motor Integration to ensure accurate gate positioning and efficient controlc:\Users\nidhi\OneDrive\Desktop\Resume nidhi editing.pdf.
+"
+              ghLink="https://github.com/coding-geek-nm/automatic_railwaygate_handler"
+              // demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={chatroom}
+              isBlog={false}
+              title="Chatroom"
+              description="•	Built a Real-Time Chatroom using React and Firebase, enabling users to chat instantly with mail ID authentication for secure access.
+•	Integrated Firebase Realtime Database to manage live message updates and user presence, providing a dynamic and responsive chat environment. 
+"
+              ghLink="https://github.com/coding-geek-nm/chatroom"
+              // demoLink="https://editor.soumya-jit.tech/"              
+            />
+          </Col>
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={leaf}
+              isBlog={false}
+              title="Plant AI"
+              description="Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model."
+              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col> */}
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={suicide}
+              isBlog={false}
+              title="Ai For Social Good"
+              description="Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace  and thus helping in sucide prevention."
+              ghLink="https://github.com/soumyajit4419/AI_For_Social_Good"
+              // demoLink="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley" <--------Please include a demo link here
+            />
+          </Col> */}
+
+          {/* <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={emotion}
+              isBlog={false}
+              title="Face Recognition and Emotion Detection"
+              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
+              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
+              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
+              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
+            />
+          </Col> */}
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
